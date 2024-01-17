a7ec4b36dc5cc5636b9f351502f18b0339756dd3
Aishah <aishah122005@gmail.com>
2024-01-17 09:43:03 -0600

    Initial Commit

---
+++ country flag hackwhich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  country flag hackwhich
+//
+//  Created by Aishah Siraj on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ country flag hackwhich/country_flag_hackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  country_flag_hackwhichApp.swift
+//  country flag hackwhich
+//
+//  Created by Aishah Siraj on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct country_flag_hackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
